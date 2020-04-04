e0e925462106bdcd93d3aeea320c5f47b68e53c4
Sandie Nunez <snunez@vt.edu>
2020-04-04 03:19:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,10 @@
 require "pry"
 def my_collect(names)
-  i = names
-  yield(names[i])
-
-  end 
+  i = 0
+ students2 = []
+  while i < students.length
+     students2 << yield(students[i])
+    i += 1
+  end
+   students2
 end
